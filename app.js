9a3d7d8962d1760dd85fc9e6bdeda523c3af72a8
Salie Lim <salie.lim@gmail.com>
2017-09-22 19:02:52 +0800

    Add styling, improve UI

--- app.js
+++ app.js
@@ -15,16 +15,37 @@ $(document).ready(function(){
         for(var i=0; i<data.items.length; i++) {
           var ownerName = data.items[i].full_name;
           var language = data.items[i].language;
+          var followers = data.items[i].watchers;
           var url = data.items[i].html_url;
           var description = data.items[i].description;
 
-          $("#search-result").prepend(
+          $("#search-result").append(
             `<div class="results">
-              <h4><a class="owner">${ ownerName }</a></h4>
+              <h4>
+                <a class="owner">
+                  ${ ownerName }
+                  <span class="glyphicon glyphicon glyphicon-info-sign pull-right" aria-hidden="true"></span>
+                </a>
+              </h4>
+              
               <div class="details hidden">
-                <b>Language: </b>${ language }
-                </br><b>URL: </b>${ url }
-                </br><b>Description: </b>${ description }
+                <hr>
+                <a href="${ url }" target="blank">
+                  ${ url } 
+                  <span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+                </a>
+                </br>
+                ${ description }
+                </br>
+                <hr>
+                <p class="pull-left">
+                  ${ language }
+                </p>
+                <p class="pull-right">
+                  &nbsp${ followers }
+                </p>
+                <span class="glyphicon glyphicon-eye-open
+                pull-right"></span>
                 </br>
               </div>
             </div>`
@@ -34,10 +55,10 @@ $(document).ready(function(){
         $(".results").click(function(){
           $(this).children().eq(1).toggleClass( "hidden" );
         });
-     },
+      },
       error: function(errorMessage){
-        $("#search-result").prepend(
-          "Sorry, there's an error. </br> Github API might be down.."
+        $("#search-result").append(
+          "Sorry there's an error, GitHub API might be down.."
         )
       }
     })
