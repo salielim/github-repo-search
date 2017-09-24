a2133d494b6e0f80a369e58f1cc3288643773544
Salie Lim <salie.lim@gmail.com>
2017-09-24 18:35:41 +0800

    Get list of languages for each repo

--- app.js
+++ app.js
@@ -1,49 +1,50 @@
-$(document).ready(function(){
+$(document).ready(function () {
   $('#loading-div').hide();
 
   // Disable search button if input is empty
-  $("#search-button").attr("disabled",true);
-  $("#search-query").keyup(function(){
-    if($(this).val().length !=0)
-        $("#search-button").attr('disabled', false);            
+  $("#search-button").attr("disabled", true);
+  $("#search-query").keyup(function () {
+    if ($(this).val().length != 0)
+      $("#search-button").attr('disabled', false);
     else
-        $("#search-button").attr('disabled',true);
+      $("#search-button").attr('disabled', true);
   })
 
   // Get data using keyword
-  $("#search-button").click(function(){
+  $("#search-button").click(function () {
     var searchQuery = $("#search-query").val();
     var url = "https://api.github.com/search/repositories?q=" + searchQuery;
 
     $("#result-count").html("");
     $("#search-result").html("");
-    
+
     $.ajax({
       type: "GET",
       url: url,
       async: true,
       dataType: "json",
-      beforeSend: function() { $('#loading-div').show(); },
-      complete: function() { $('#loading-div').hide(); },
-      success: function(data) {
-        // Show no. of results
+      beforeSend: function () { $('#loading-div').show(); },
+      success: function (data) {
         var totalCount = data.total_count.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+
+        // Show no. of results
         $("#result-count").html(
           `<small class="text-muted">
-            Showing 30 of ${ totalCount } results
+            Top 30 of ${ totalCount} results
           </small>`
         );
 
         // Loop each search result
-        for(var i=0; i<data.items.length; i++) {
-          
+        for (var i = 0; i < data.items.length; i++) {
+
           var ownerName = data.items[i].full_name;
           var language = data.items[i].language;
-          var followers = data.items[i].watchers;
+          var followers = data.items[i].watchers.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
           var url = data.items[i].html_url;
           var description = data.items[i].description;
+          var languagesUrl = data.items[i].languages_url;
 
-          // If value of language & description are null
+          // If values are null show no language / description
           if (language === null || language === undefined) {
             var language = "No language";
           }
@@ -52,48 +53,71 @@ $(document).ready(function(){
             var description = "No description";
           }
 
-          // Append info
+          // 2nd ajax call for list of languages
+          $.ajax({
+            type: "GET",
+            url: languagesUrl,
+            async: true,
+            dataType: "json",
+            success: function (languagesData) {
+
+              var languages = Object.keys(languagesData);
+
+              console.log(languages);
+
+              // Append languages
+              // ...
+            },
+            error: function (errorMessage) {
+              $("#search-result").html(
+                "Sorry, GitHub API might be down.."
+              )
+            }
+          });
+
+          // Append repo info
           $("#search-result").append(
             `<div class="results">
-              <h4>
-                <a class="owner">
-                  ${ ownerName }
-                  <span class="glyphicon glyphicon glyphicon-info-sign pull-right" aria-hidden="true"></span>
-                </a>
-              </h4>
-              
-              <div class="details hidden">
-                <hr>
-                <a href="${ url }" target="blank">
-                  ${ url } 
-                  <span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
-                </a>
+                <h4>
+                  <a class="owner">
+                    ${ownerName}
+                    <span class="glyphicon glyphicon glyphicon-info-sign pull-right" aria-hidden="true"></span>
+                  </a>
+                </h4>
+
+                <div class="details hidden">
+                  <hr>
+                    <a href="${url}" target="blank" class="repo-url">
+                      ${url}
+                      <span class="glyphicon glyphicon-new-window" aria-hidden="true"></span>
+                    </a>
                 </br>
-                ${ description }
+                  ${description}
                 </br>
                 <hr>
-                <p class="pull-left">
-                  ${ language }
-                </p>
-                <p class="pull-right">
-                  &nbsp${ followers }
-                </p>
-                <span class="glyphicon glyphicon-eye-open
-                pull-right"></span>
+                  <span class="label label-default pull-left x">
+
+                  </span>
+                  <p class="pull-right">
+                    &nbsp${followers}
+                  </p>
+                  <span class="glyphicon glyphicon-eye-open
+                    pull-right"></span>
                 </br>
               </div>
-            </div>`
+            </div >`
           );
         };
-        
+
         // Toggle repo cards
-        $(".results").click(function(){
-          $(this).children().eq(1).toggleClass( "hidden" );
+        $(".results").click(function () {
+          $(this).children().eq(1).toggleClass("hidden");
         });
       },
-      error: function(errorMessage){
+      complete: function () { $('#loading-div').hide(); },
+      error: function (errorMessage) {
         $("#search-result").html(
-          "Sorry there's an error, GitHub API might be down.."
+          "Sorry, GitHub API might be down.."
         )
       }
     })
